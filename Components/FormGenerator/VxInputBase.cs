e8f55c435eccfd485c069bb7bce3143e5ea9ebb3
Alex <aaltuj@live.nl>
2020-07-08 23:09:53 +0200

    splitted checkbox and bootstrap version

---
+++ Components/FormGenerator/VxInputBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+
+namespace FormGeneratorDemo.Components.FormGenerator
+{
+    public abstract class VxInputBase<TValue> : InputBase<TValue>
+    {
+        private string id = Guid.NewGuid().ToString();
+
+        [Parameter] public string Id { get => id; set => id = value; }
+    }
+}
--- Components/FormGenerator/VxInputCheckbox.razor.cs
+++ Components/FormGenerator/VxInputCheckbox.razor.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 
 namespace FormGeneratorDemo.Components.FormGenerator
 {
-    public class BootstrapInputCheckboxComponent : VxInputBase<bool>
+    public class VxInputCheckboxComponent : VxInputBase<bool>
     {
-        public string Label { get; set; }
+        [Parameter] public string Label { get; set; }
+        [Parameter] public string LabelCss { get; set; }
+        [Parameter] public string ContainerCss { get; set; }
 
         protected override bool TryParseValueFromString(string value, out bool result, out string validationErrorMessage)
            => throw new NotImplementedException($"This component does not parse string inputs. Bind to the '{nameof(CurrentValue)}' property, not '{nameof(CurrentValueAsString)}'.");
