fc7db672333d9aaf87c1ee26c8fa812c43f2250d
Alex <aaltuj@live.nl>
2020-07-14 23:09:44 +0200

    added comments

--- Components/FormGenerator/FormElement.razor.cs
+++ Components/FormGenerator/FormElement.razor.cs
@@ -16,12 +16,27 @@ namespace FormGeneratorDemo.Components.FormGenerator
     {
         private string _Label;
         private FormGeneratorComponentsRepository _repo;
+        /// <summary>
+        /// Bindable property to set the class
+        /// </summary>
         public string CssClass { get => string.Join(" ", CssClasses.ToArray()); }
+        /// <summary>
+        /// Setter for the classes of the form container
+        /// </summary>
         [Parameter] public List<string> CssClasses { get; set; }
-        [Parameter] public List<string> DefaultFieldClasses { get; set; }
 
+        /// <summary>
+        /// Will set the 'class' of the all the controls. Useful when a framework needs to implement a class for all form elements
+        /// </summary>
+        [Parameter] public List<string> DefaultFieldClasses { get; set; }
+        /// <summary>
+        /// The identifier for the <see cref="FormElement"/>"/> used by the label element
+        /// </summary>
         [Parameter] public string Id { get; set; }
 
+        /// <summary>
+        /// The label for the <see cref="FormElement"/>, if not set, it will check for a <see cref="DisplayAttribute"/> on the <see cref="CascadedEditContext.Model"/>
+        /// </summary>
         [Parameter]
         public string Label
         {
@@ -38,23 +53,37 @@ namespace FormGeneratorDemo.Components.FormGenerator
             set { _Label = value; }
         }
 
+        /// <summary>
+        /// The property that should generate a formcontrol
+        /// </summary>
         [Parameter] public PropertyInfo FieldIdentifier { get; set; }
 
+        /// <summary>
+        /// Get the <see cref="EditForm.EditContext"/> instance. This instance will be used to fill out the values inputted by the user
+        /// </summary>
         [CascadingParameter] EditContext CascadedEditContext { get; set; }
 
         protected override void OnInitialized()
         {
+            // setup the repo containing the mappings
             _repo = ScopedServices.GetService(typeof(FormGeneratorComponentsRepository)) as FormGeneratorComponentsRepository;
         }
 
+        /// <summary>
+        /// A method thar renders the form control based on the <see cref="FormElement.FieldIdentifier"/>
+        /// </summary>
+        /// <param name="propInfoValue"></param>
+        /// <returns></returns>
         public RenderFragment CreateComponent(System.Reflection.PropertyInfo propInfoValue) => builder =>
         {
-
+            // Get the mapped control based on the property type
             var componentType = _repo.GetComponent(propInfoValue.PropertyType.ToString());
 
-            if (componentType == null)
-                return;
 
+            if (componentType == null)
+                throw new Exception($"No component found for: {propInfoValue.PropertyType.ToString()}");
+
+            // Set the found component
             var elementType = componentType;
 
             // When the elementType that is rendered is a generic Set the propertyType as the generic type
@@ -62,7 +91,6 @@ namespace FormGeneratorDemo.Components.FormGenerator
             {
                 Type[] typeArgs = { propInfoValue.PropertyType };
                 elementType = elementType.MakeGenericType(typeArgs);
-
             }
 
             // Activate the the Type so that the methods can be called
@@ -80,23 +108,23 @@ namespace FormGeneratorDemo.Components.FormGenerator
         /// </summary>
         /// <typeparam name="T">The type of the property</typeparam>
         /// <typeparam name="TElement">The type of the form element, should be based on <see cref="InputBase{TValue}"/>, like a <see cref="InputText"/></typeparam>
-        /// <param name="target"></param>
-        /// <param name="dataContext"></param>
-        /// <param name="propInfoValue"></param>
-        /// <param name="builder"></param>
-        /// <param name="instance"></param>
+        /// <param name="target">This <see cref="FormElement"/> </param>
+        /// <param name="dataContext">The Model instance</param>
+        /// <param name="propInfoValue">The property that is being rendered</param>
+        /// <param name="builder">The render tree of this element</param>
+        /// <param name="instance">THe control instance</param>
         public void CreateFormComponent<T, TElement>(object target,
             object dataContext,
             PropertyInfo propInfoValue, RenderTreeBuilder builder, InputBase<T> instance)
         {
-
+            // Create the component based on the mapped Element Type
             builder.OpenComponent(0, typeof(TElement));
 
             // Bind the value of the input base the the propery of the model instance
             var s = propInfoValue.GetValue(dataContext);
             builder.AddAttribute(1, nameof(InputBase<T>.Value), s);
 
-            // Create the handler for ValueChanged. I use reflection to the value.
+            // Create the handler for ValueChanged. This wil update the model instance with the input
             builder.AddAttribute(3, nameof(InputBase<T>.ValueChanged),
                     Microsoft.AspNetCore.Components.CompilerServices.RuntimeHelpers.TypeCheck(
                         EventCallback.Factory.Create<T>(
@@ -110,6 +138,7 @@ namespace FormGeneratorDemo.Components.FormGenerator
             var lamb = Expression.Lambda<Func<T>>(exp);
             builder.AddAttribute(4, nameof(InputBase<T>.ValueExpression), lamb);
 
+            // Set the class for the the formelement.
             builder.AddAttribute(5, "class", GetDefaultFieldClasses(instance));
 
             CheckForInterfaceActions<T, TElement>(this, CascadedEditContext.Model, propInfoValue, builder, instance, 6);
@@ -122,21 +151,28 @@ namespace FormGeneratorDemo.Components.FormGenerator
             object dataContext,
             PropertyInfo propInfoValue, RenderTreeBuilder builder, InputBase<T> instance, int indexBuilder)
         {
-
+            // overriding the default classes for FormElement
             if (TypeImplementsInterface(typeof(TElement), typeof(IRenderAsFormElement)))
             {
                 this.CssClasses.AddRange((instance as IRenderAsFormElement).FormElementClasses);
             }
 
-            // Check if the component has the IRenderChildren and read the type of component that should be rendered
+            // Check if the component has the IRenderChildren and renderen them in the form control
             if (TypeImplementsInterface(typeof(TElement), typeof(IRenderChildren)))
             {
                 (instance as IRenderChildren).RenderChildren(builder, indexBuilder, dataContext, propInfoValue);
             }
         }
 
+        /// <summary>
+        /// Merges the default control classes with the <see cref="InputBase{TValue}.AdditionalAttributes"/> 'class' key
+        /// </summary>
+        /// <typeparam name="T">The property type of the formelement</typeparam>
+        /// <param name="instance">The instance of the component representing the form control</param>
+        /// <returns></returns>
         private string GetDefaultFieldClasses<T>(InputBase<T> instance)
         {
+
             var output = DefaultFieldClasses == null ? "" : string.Join(" ", DefaultFieldClasses);
 
             if (instance == null)
