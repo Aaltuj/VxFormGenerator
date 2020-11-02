011352b5d6fb3b1ef7d520554ea0c8d7cbd07ab4
Alex <aaltuj@live.nl>
2020-11-02 08:21:05 +0100

    Added CssClassProvider for bootstrap

---
+++ VxFormGenerator.Components.Bootstrap/VxBootstrapFormValidationCssClassProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VxFormGenerator.Settings.Bootstrap
+{
+    public class VxBootstrapFormValidationCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var cssClassName = base.GetFieldCssClass(editContext, fieldIdentifier);
+
+            // If we can find a [CustomValidationClassName], use it
+            var propertyInfo = fieldIdentifier.Model.GetType().GetProperty(fieldIdentifier.FieldName);
+            if (propertyInfo != null)
+            {
+                var customValidationClassName = (VxFormValidationCssClassAttribute)propertyInfo
+                    .GetCustomAttributes(typeof(VxFormValidationCssClassAttribute), true)
+                    .FirstOrDefault();
+                if (customValidationClassName != null)
+                {
+                    cssClassName = string.Join(' ', cssClassName.Split(' ').Select(token => token switch
+                    {
+                        "valid" => customValidationClassName.Valid ?? token,
+                        "invalid" => customValidationClassName.Invalid ?? token,
+                        _ => token,
+                    }));
+                }
+            }
+
+            return cssClassName;
+        }
+    }
+}
---
+++ VxFormGenerator.Components.Plain/VxFormValidationCssClassAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VxFormGenerator.Settings
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class VxFormValidationCssClassAttribute: Attribute
+    {   
+        public string Valid { get; set; }
+        public string Invalid { get; set; }
+    }
+}
