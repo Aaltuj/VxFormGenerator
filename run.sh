b520ed15967e744d3fc86f305c8b8a04c07a101e
Alex <aaltuj@live.nl>
2021-02-01 21:55:47 +0100

    added bash script

--- run.sh
+++ run.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-dotnet watch --project ./Demo/FormGeneratorDemo.csproj run --configuration Bootstrap
+PS3="Select framework: "
+select i in Bootstrap Plain exit
+do
+  case $i in
+    Bootstrap) 
+    FRAMEWORK="--configuration Bootstrap" 
+    break
+    ;;
+    Plain) 
+    FRAMEWORK=""
+    break
+    ;;
+    exit) exit;;
+  esac
+done
+
+PS3="Select kind of application: "
+select i in Server Wasm exit
+do
+  case $i in
+    Server) dotnet watch --project ./VxFormGeneratorDemo.Server/VxFormGeneratorDemo.Server.csproj run $FRAMEWORK 
+    break
+    ;;
+    Wasm) dotnet watch --project ./VxFormGeneratorDemo.Wasm/VxFormGeneratorDemo.Wasm.csproj run $FRAMEWORK
+    break
+    ;;
+    exit) exit;;
+  esac
+done
+
+#dotnet watch --project ./Demo/FormGeneratorDemo.csproj run --configuration Bootstrap
